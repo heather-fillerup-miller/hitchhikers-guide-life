160034afd043cf81e319f055347881f01323ee58
Heather <32920594+heather-fillerup-miller@users.noreply.github.com>
2022-07-08 21:40:48 -0500

    Updated Folder Directory

--- src/Guide/Profile/index.js
+++ src/Guide/Profile/index.js
@@ -56,7 +56,7 @@ const links = [
   }
 ];
 
-const SettingsDashboard = () => {
+const ProfileDashboard = () => {
   const [optionCountry, setOptionCountry] = React.useState(optionsCountries[0]);
   const [optionCity, setOptionCity] = React.useState(optionsCities[0]);
 
@@ -240,4 +240,4 @@ const SettingsDashboard = () => {
   );
 };
 
-export default SettingsDashboard;
+export default ProfileDashboard;
--- src/Guide/index.js
+++ src/Guide/index.js
@@ -4,7 +4,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from './Home';
 import DocumentationPage from './DocumentationPage';
 import SignUp from './SignUp';
-import UserSettings from './UserSettings';
+import Profile from './Profile';
 import Login from './Login';
 
 export const Guide = () => {
@@ -14,7 +14,7 @@ export const Guide = () => {
       <Route component={SignUp} exact path="/guide/sign-up" strict />
       <Route component={Home} exact path="/guide/home" strict />
       <Route component={DocumentationPage} exact path="/guide/doc" strict />
-      <Route component={UserSettings} exact path="/guide/settings" strict />
+      <Route component={Profile} exact path="/guide/settings" strict />
       <Route component={Login} exact path="/guide/login" strict />
       <Redirect to="/guide/" />
     </Switch>
