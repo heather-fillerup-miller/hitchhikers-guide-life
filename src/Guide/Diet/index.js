ab777b56bc4026b31dffec6c95a35a28214c37e8
Heather <32920594+heather-fillerup-miller@users.noreply.github.com>
2022-07-31 13:00:38 -0500

    Integrated Partner's Microservice

--- src/Guide/Diet/index.js
+++ src/Guide/Diet/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import classnames from 'classnames';
 import { UiContext } from '@context';
 import { Link } from 'react-router-dom';
@@ -9,20 +9,82 @@ import {
   ContainerHorizontal,
   ScrollArea
 } from '@duik/it';
-
+import { BarLoader } from 'react-spinners';
 import { DietTopic, WidgetVideoPreview, WidgetLink, Header } from '@components';
 
 import cls from './diet-home.module.scss';
 
-import meals from './assets/meals';
 import sidebarNavigation from './assets/sidebarNavigation';
 import sidebarVideos from './assets/sidebarVideos';
 
 const Diet = ({ isMenuOpen }: { isMenuOpen: Boolean }) => {
-   
-  const 
 
+  const [ microServiceMeals, setMicroServiceMeals ] = useState(null);
+  const [ isLoading, setIsLoading ] = useState(true);
   const uiContext = React.useContext(UiContext);
+
+  // fetch three meals from the recipe microservice
+  const fetchMeals = useCallback(async () => {
+    console.log('fetching recipes');
+    let fetchedMeals = [];
+    try {
+      let response = await fetch('http://localhost:9000/?tags=breakfast');
+      if (response.ok) {
+        const breakfastItem = (await response.json()).map(item => ({
+          mealTitle: "Breakfast",
+          id: item.id,
+          title: item.title,
+          healthScore: item.healthscore,
+          sourceUrl: item.spoonacularSourceUrl,
+          imgUrl: item.imgUrl,
+          letsDoItUrl: '/guide/profile',
+          creditText: item.creditText
+        }));
+        fetchedMeals.push(breakfastItem[0]);
+        response = await fetch('http://localhost:9000/?tags=lunch');
+        if(response.ok) {
+          const lunchItem = (await response.json()).map(item => ({
+            mealTitle: "Lunch",
+            id: item.id,
+            title: item.title,
+            healthScore: item.healthscore,
+            sourceUrl: item.spoonacularSourceUrl,
+            imgUrl: item.imgUrl,
+            letsDoItUrl: '/guide/profile',
+            creditText: item.creditText
+          }));
+          fetchedMeals.push(lunchItem[0]);
+          response = await fetch('http://localhost:9000/?tags=dinner');
+          if (response.ok) {
+            const dinnerItem = (await response.json()).map(item => ({
+              mealTitle: "Dinner",
+              id: item.id,
+              title: item.title,
+              healthScore: item.healthscore,
+              sourceUrl: item.spoonacularSourceUrl,
+              imgUrl: item.imgUrl,
+              letsDoItUrl: '/guide/profile',
+              creditText: item.creditText
+            }));
+            fetchedMeals.push(dinnerItem[0]);
+            console.log("Meals: " + JSON.stringify(fetchedMeals));
+          } 
+        }
+      }
+    } catch(err) {
+      console.error(err);
+    } finally {
+      setMicroServiceMeals(fetchedMeals);
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if(isLoading) {
+      fetchMeals();
+    }
+  },[isLoading, fetchMeals]);
+
   return (
     <ContainerVertical>
       <Header isHome={false}/>
@@ -35,7 +97,8 @@ const Diet = ({ isMenuOpen }: { isMenuOpen: Boolean }) => {
               </h2>
               <p><Link to='/guide/home'>Home</Link> / About /<Link to='/guide/diet'> Meals</Link> / Fitness / Stress Relief / Finances</p>
               <div className={cls['diet-list']}>
-                {meals.map(item => (
+                {isLoading && <BarLoader/>}
+                {!isLoading && microServiceMeals.map(item => (
                   <div key={item.title}>
                     <DietTopic key={item.title} {...item} />
                   </div>
